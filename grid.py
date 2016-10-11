2138bdc17929c3482c24044ab2c7bdc4b933f2e6
Aradhya Makkar <aradhyamakkar97@gmail.com>
2016-10-12 01:10:15 +0530

    adv. grid stuff

--- grid.py
+++ grid.py
@@ -3,12 +3,18 @@ from tkinter import ttk
 
 root = Tk()
 
-Label(root,text="First Name").grid(row=0,sticky=W,padx=4)
-Entry(root).grid(row=0,column=1,sticky=E,pady=4)
+Label(root,text="Description").grid(row=0,column=0,sticky=W)
+Entry(root,width=50).grid(row=0,column=1)
+Button(root,text="Submit").grid(row=0,column=8)
 
-Label(root,text="Last Name").grid(row=1,sticky=W,padx=4)
-Entry(root).grid(row=1,column=1,sticky=E,pady=4)
+Label(root,text="Quality").grid(row=1,column=0,sticky=W)
+Radiobutton(root,text="new",value=1).grid(row=2,column=0,sticky=W)
+Radiobutton(root,text="Good",value=2).grid(row=3,column=0,sticky=W)
+Radiobutton(root,text="Poor",value=3).grid(row=4,column=0,sticky=W)
+Radiobutton(root,text="Damaged",value=4).grid(row=5,column=0,sticky=W)
 
-Button(root,text="Submit").grid(row=3)
 
+Label(root,text="Benifits").grid(row=1,column=1,sticky=W)
+Checkbutton(root,text="Free Shipping").grid(row=2,column=1,sticky=W)
+Checkbutton(root,text="Bonus Gift").grid(row=3,column=1,sticky=W)
 root.mainloop()
