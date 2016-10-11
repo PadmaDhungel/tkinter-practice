2a93a9b2a3f769195690d36d665298567b27b6da
Aradhya Makkar <aradhyamakkar97@gmail.com>
2016-10-12 01:26:40 +0530

    grid.py- add calculator

--- grid.py
+++ grid.py
@@ -1,20 +1,36 @@
 from tkinter import *
 from tkinter import ttk
 
+def get_sum(event):
+    num1 = int(num1Entry.get())
+    num2 = int(num2Entry.get())
+
+    s = num1 + num2
+
+    sumEntry.delete(0,"end")
+
+    sumEntry.insert(0,s)
+
+
+
 root = Tk()
 
-Label(root,text="Description").grid(row=0,column=0,sticky=W)
-Entry(root,width=50).grid(row=0,column=1)
-Button(root,text="Submit").grid(row=0,column=8)
+num1Entry = Entry(root)
+num1Entry.pack(side=LEFT)
 
-Label(root,text="Quality").grid(row=1,column=0,sticky=W)
-Radiobutton(root,text="new",value=1).grid(row=2,column=0,sticky=W)
-Radiobutton(root,text="Good",value=2).grid(row=3,column=0,sticky=W)
-Radiobutton(root,text="Poor",value=3).grid(row=4,column=0,sticky=W)
-Radiobutton(root,text="Damaged",value=4).grid(row=5,column=0,sticky=W)
+Label(root,text="+").pack(side=LEFT)
+
+num2Entry = Entry(root)
+num2Entry.pack(side=LEFT)
+
+equalButton = Button(root,text="=")
+equalButton.bind("<Button-1>",get_sum)
+equalButton.pack(side=LEFT)
+
+sumEntry = Entry(root)
+sumEntry.pack(side=LEFT)
 
 
-Label(root,text="Benifits").grid(row=1,column=1,sticky=W)
-Checkbutton(root,text="Free Shipping").grid(row=2,column=1,sticky=W)
-Checkbutton(root,text="Bonus Gift").grid(row=3,column=1,sticky=W)
+
+
 root.mainloop()
