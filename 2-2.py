dc3216df96d476e7f78811e6ddcdff5a4d418c7c
Aradhya Makkar <aradhyamakkar97@gmail.com>
2016-10-14 13:37:25 +0530

    new stuff

--- 2-2.py
+++ 2-2.py
@@ -22,6 +22,40 @@ file_menu.add_command(label="Quit",command=quit_app)
 
 the_menu.add_cascade(label="File",menu=file_menu)
 
+text_font = StringVar()
+text_font.set("Times")
+
+def change_font(event=None):
+    print('font picked: ',text_font.get())
+
+font_menu = Menu(the_menu,tearoff=0)
+
+font_menu.add_radiobutton(label="Times",variable=text_font,command=change_font)
+
+
+view_menu = Menu(the_menu,tearoff=0)
+
+line_numbers = IntVar()
+line_numbers.set(1)
+
+view_menu.add_checkbutton(label="Line Numbers",variable = line_numbers)
+
+view_menu.add_cascade(label="fonts",menu=font_menu)
+
+the_menu.add_cascade(label="view",menu=view_menu)
+
+
+help_menu = Menu(the_menu,tearoff=0)
+
+help_menu.add_command(label="About",accelerator="Command-A",command=show_about)
+
+the_menu.add_cascade(label="help",menu=help_menu)
+
+root.bind('<Command-A>',show_about)
+root.bind('<Command-a>',show_about)
+
+
+
 root.config(menu=the_menu)
 
 
